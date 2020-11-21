72e258bf7724e583202febfc5d21ca1d80d82e56
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2020-11-21 23:53:44 +0530

    audio Stenography

---
+++ audio/extract.py
@@ -0,0 +1,20 @@
+# python -m pysstv /path/to/image.jpg /path/to/audio.wav
+
+
+# Use wave package (native to Python) for reading the received audio file
+import wave
+datain = input('Enter the path of wave file:')
+song = wave.open(datain, mode='rb')
+# Convert audio to byte array
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+
+# Extract the LSB of each byte
+extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+# Convert byte array back to string
+string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+# Cut off at the filler characters
+decoded = string.split("###")[0]
+
+# Print the extracted text
+print("Sucessfully decoded: "+decoded)
+song.close()
---
+++ pt1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL  import Image
+import cv2
+
+val = [0  for i in range(3000000)]
+datain = np.array(val, dtype=np.int8)
+# print(datain)
+datam = datain.reshape(1000, 1000, 3)
+# print(datam)
+img = Image.fromarray(datam, 'RBG')
+img.show()
