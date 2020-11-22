b1b0646c8dc46255179bd929cb7a20a36a8ecfe8
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2020-11-23 01:12:07 +0530

    testing sstv decoding

--- audio/extract.py
+++ audio/extract.py
@@ -11,7 +11,7 @@ frame_bytes = bytearray(list(song.readframes(song.getnframes())))
 # Extract the LSB of each byte
 extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
 # Convert byte array back to string
-string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+string = "".join(chr(int("".join(map(str, extracted[i:i+8])), 2)) for i in range(0, len(extracted), 8))
 # Cut off at the filler characters
 decoded = string.split("###")[0]
 
