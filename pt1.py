405c70d71244c9264d3f9f21579d24bccc5cd813
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2020-11-25 15:23:42 +0530

    modules for text steno

--- pt1.py
+++ pt1.py
@@ -1,10 +1,25 @@
 import numpy as np
 from PIL import Image
 
-data = input('Enter your message:')
-datalist = [ord(i) for i in data]
 val = [150 for i in range(3000000)]
-datain = np.array(val, dtype=np.int8)
-datam = datain.reshape(1000, 1000, 3)
-img = Image.fromarray(datam, mode='RGB')
+array = np.array(val, dtype=np.int8).reshape(1000, 1000, 3)
+total_pixels = array.size//3
+
+data = input('Enter your message:')
+mess = "".join([format(ord(i), '08b') for i in data])
+req_pixels = len(mess)
+
+if req_pixels > total_pixels:
+    print("ERROR! File's size is not sufficient for your message.")
+    quit()
+else:
+    index = 0
+    for j in range(total_pixels):
+        for k in range(0, 3):
+            if index < req_pixels:
+                array[j][k] = int(bin(array[j][k])[2:9] + data[index], 2)
+                index += 1
+
+array2 = array.reshape(1000, 1000, 3)
+img = Image.fromarray(array, mode='RGB')
 img.show()
---
+++ sten/text.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+
+def size(file: str):
+    cmd = subprocess.Popen(['snow', '-S', file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = cmd.communicate()
+    return str(stdout, 'utf-8').split()[-2]
+
+
+def encode(passwd: str, infile: str, outfile: str, file: str = None, message: str = None):
+    if message is not None:
+        command = 'snow -C -Q -p "{}" -m "{}" {} {}'.format(passwd, message, infile, outfile)
+        os.system('cmd /c' + command)
+    elif file is not None:
+        command = 'snow -C -Q -p "{}" -f {} {} {}'.format(passwd, file, infile, outfile)
+        os.system('cmd /c' + command)
+
+
+def decode(passwd: str, file: str):
+    cmd = subprocess.Popen(['snow', '-C', '-p', passwd, file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = cmd.communicate()
+    return str(stdout, 'utf-8')
+
